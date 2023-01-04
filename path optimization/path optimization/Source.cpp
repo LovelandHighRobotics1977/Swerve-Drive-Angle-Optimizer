e720da5c1f1fd4772bd987bbea79b5884861052f
JoshCircenis <98178221+JoshCircenis@users.noreply.github.com>
2023-01-04 14:11:39 -0700

    Swerve Drive Angle Optimization
    This algorithm will choose the most efficient direction to move around a circle to replicate the desired angle given two inputs. This allows for better and smoother swerve drive because it can travel around the circle past the sensor transitions and can reverse the motor direction so that the max distance moved is only 90 degrees unless spinning while moving forward in a "tornado" maneuver. This algorithm takes the current angle of the wheel and the setpoint for the wheel to determine the best angle in sensor units and the wheel direction.

---
+++ path optimization/path optimization/Source.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int desired;
+	int current;
+	int direction = 1;
+
+
+
+	cout << "Enter current angle: ";
+	cin >> current;
+	cout << endl;
+	cout << "Enter desired angle: ";
+	cin >> desired;
+	cout << endl;
+
+
+
+	int angle1 = (desired - (current % 2048));
+	int angle2 = ((2048 - abs(desired % 2048)) + (2048 - abs(current % 2048)));
+	int shortest;
+	int check1;
+	
+
+	if (abs(angle2) < abs(angle1)) {
+		shortest = angle2;
+		check1 = true;
+	}
+	else {
+		shortest = angle1;
+		check1 = false;
+	}
+
+	angle1 = ((desired - 2048) - (current % 2048));
+	angle2 = ((2048 - (abs(desired % 2048)-2048)) + (2048 - (abs(current % 2048))));
+	
+
+	int shortest2;
+	bool check2;
+	if (abs(angle2) < abs(angle1)) {
+		shortest2 = angle2;
+		check2 = true;
+	}
+	else {
+		shortest2 = angle1;
+		check2 = false;
+	}
+
+
+	if (abs(shortest) <= abs(shortest2)) {
+		if (check1 == true) {
+			if (current < 0) {
+				current -= shortest;
+			}
+			else {
+				current += shortest;
+			}
+		}
+		else {
+			current += shortest;
+		}
+	}
+	else {
+		direction = -1;
+		if (check2 == true) {
+			if (current < 0) {
+				current -= shortest2;
+			}
+			else {
+				current += shortest2;
+			}
+		}
+		else {
+			current += shortest2;
+		}
+	}
+
+	cout << "Final angle: " << current << " Direction: " << direction << endl;
+	system("pause");
+	system("CLS");
+	main();
+}
